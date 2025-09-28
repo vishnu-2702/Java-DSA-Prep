c851e89b6286bbc1e2e3e204e8ebf80d486ebfe1
vishnu-2702 <vishnu.v2704@gmail.com>
2025-09-28 18:21:26 +0530

    new file

---
+++ conditions and loop/FibonacciNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FibonacciNumber {
+
+    public static void fibonacci(int n){
+        int a = 0;
+        int b = 1;
+
+        for(int i = 0; i <= n; i++){
+            System.out.print(a + " ");
+            int temp = a+b;
+            a = b;
+            b = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        fibonacci(n);
+    }
+}
